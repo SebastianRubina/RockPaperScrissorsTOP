41bac32e3e18413b81c1cffd50a47f7b671af753
SebastianRubina <sebastian.rubina@icloud.com>
2022-02-26 15:25:29 -0500

    Added basic UI

--- js/js.js
+++ js/js.js
@@ -1,6 +1,5 @@
 function computerPlay() {
   let computerChoice = Math.random() * 3;
-  console.log(computerChoice);
   if (computerChoice < 1) {
     return "rock";
   } else if (computerChoice < 2) {
@@ -13,23 +12,21 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
   let winner;
   if (playerSelection == computerSelection) {
-    console.log("It's a tie!");
     winner = "tie";
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    console.log("Rock beats Scissors, you win!");
     winner = "player";
   } else if (playerSelection == "rock" && computerSelection == "paper") {
-    console.log("Paper beats Rock, you lose!");
     winner = "computer";
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    console.log("Paper beats Rock, you win!");
     winner = "player";
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    console.log("Paper beats Rock, you win!");
-    winner = "player"
+    winner = "player";
   } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    console.log("Scissors beat Paper, you lose!");
-    winner = "computer"
+    winner = "computer";
+  } else if (playerSelection == 'scissors' && computerSelection == "rock") {
+    winner = "computer";
+  } else if (playerSelection == 'scissors' && computerSelection == "paper") {
+    winner = "player";
   }
   return winner;
 }
@@ -62,5 +59,32 @@ function game() {
 }
 
 
-game()
 
+
+let prock = document.querySelector('.prock');
+let ppaper = document.querySelector('.ppaper');
+let pscissors = document.querySelector('.pscissors');
+let winnerMsg = document.querySelector('.winner');
+let playerChoice = document.querySelector('.playerChoice');
+let computerChoice = document.querySelector('.computerChoice');
+let buttons = document.querySelectorAll('button');
+
+
+buttons = document.addEventListener('click', testFun);
+
+function testFun(e) {
+  let playerSelection = e.target.value;
+  let computerSelection = computerPlay();
+  console.log(playerSelection);
+
+  playerChoice.innerHTML = `Player picked: ${playerSelection}`;
+  computerChoice.innerHTML = `Computer picked: ${computerSelection}`;
+
+  let winner = playRound(playerSelection, computerSelection);
+  if (winner != 'tie') {
+    winnerMsg.innerHTML = `The winner is ${winner}!`;
+  } else {
+    winnerMsg.innerHTML = 'Tie! Try again!';
+  }
+  console.log(winnerMsg);
+}
